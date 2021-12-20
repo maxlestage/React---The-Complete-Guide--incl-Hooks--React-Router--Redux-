4c65c18c04192c98e1e7805f01e503f4d44522e3
maxlestage <maxlestage@icloud.com>
2021-12-20 21:42:52 +0100

    React - The Complete Guide (incl Hooks, React Router, Redux)

---
+++ src/components/Expenses/Expense.jsx
@@ -0,0 +1,31 @@
+import Card from "../UI/Card";
+import "./Expense.css";
+import ExpendItems from "./ExpenseItems";
+
+function Expense(props) {
+  return (
+    <Card className="expenses">
+      <ExpendItems
+        title={props.items[0].title}
+        amount={props.items[0].amount}
+        date={props.items[0].date}
+      />
+      <ExpendItems
+        title={props.items[1].title}
+        amount={props.items[1].amount}
+        date={props.items[1].date}
+      />
+      <ExpendItems
+        title={props.items[2].title}
+        amount={props.items[2].amount}
+        date={props.items[2].date}
+      />
+      <ExpendItems
+        title={props.items[3].title}
+        amount={props.items[3].amount}
+        date={props.items[3].date}
+      />
+    </Card>
+  );
+}
+export default Expense;
