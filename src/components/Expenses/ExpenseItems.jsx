8b5a43d9b291d166b13429ff54a3ccdfcffec6fb
maxlestage <maxlestage@icloud.com>
2021-12-30 13:33:18 +0100

    Action de changement de titre commenté.

--- src/components/Expenses/ExpenseItems.jsx
+++ src/components/Expenses/ExpenseItems.jsx
@@ -1,19 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "./ExpenseItems.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 function ExpendItems(props) {
-  const [title, setTitle] = useState(props.title);
-  console.log("ExpenseItem evaluated by React");
-  // let title = props.title;
+  // const [title, setTitle] = useState(props.title);
+  // console.log("ExpenseItem evaluated by React");
+  // // let title = props.title;
 
-  const handlerClicker = () => {
-    // title = "Update!";
-    setTitle("Test title");
-    console.log(title);
-  };
+  // const handlerClicker = () => {
+  //   // title = "Update!";
+  //   setTitle("Test title");
+  //   console.log(title);
+  // };
 
   return (
     <li>
@@ -23,7 +23,7 @@ function ExpendItems(props) {
           <h2>{props.title}</h2>
           <div className="expense-item__price">${props.amount}</div>
         </div>
-        <button onClick={handlerClicker}>Update Title</button>
+        {/* <button onClick={handlerClicker}>Update Title</button> */}
       </Card>
     </li>
   );
