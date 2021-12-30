7637372ca81b248243a634979a251db92528e024
maxlestage <maxlestage@icloud.com>
2021-12-30 14:13:27 +0100

    add spacing

--- src/components/Expenses/ExpensesList.jsx
+++ src/components/Expenses/ExpensesList.jsx
@@ -6,6 +6,7 @@ const ExpensesList = ({ items }) => {
   if (items.length === 0) {
     return <h2 className="expenses-list__fallback">Found no Expenses.</h2>;
   }
+
   return (
     <ul className="expenses-list">
       {items.map((item) => (
